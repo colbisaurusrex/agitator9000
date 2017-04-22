59348b63cba39ced68bf0b8a35494e90d59610d8
colbisaurusrex <c.barthelmess1@gmail.com>
2017-04-22 18:33:21 -0400

    prompt user for name and address

--- withoutlibraries/app.js
+++ withoutlibraries/app.js
@@ -1,3 +1,6 @@
+const stdin = process.stdin;
+const stdout = process.stdout;
+
 const Agitator = function(){
   this.official = null;
   this.userMessage = null;
@@ -5,12 +8,45 @@ const Agitator = function(){
   this.fetchStatus = false;
   this.user = {};
   this.titles = {
-  'US House Representative': {role: 'legislatorLowerBody', level:'country'},
-  'US Congress Representative': {role: 'legislatorUpperBody', level: 'country'},
-  'State House Representative': {role: 'legislatorLowerBody', level:'administrativeArea1'},
-  'State Congress Representative': {role: 'legislatorUpperBody', level: 'administrativeArea1'},
-  Governor: {role: 'headOfGovernment', level: 'administrativeArea1' },
-  President: {role:'headOfState', level: 'country' },
-  }
+    'US House Representative': {role: 'legislatorLowerBody', level:'country'},
+    'US Congress Representative': {role: 'legislatorUpperBody', level: 'country'},
+    'State House Representative': {role: 'legislatorLowerBody', level:'administrativeArea1'},
+    'State Congress Representative': {role: 'legislatorUpperBody', level: 'administrativeArea1'},
+    Governor: {role: 'headOfGovernment', level: 'administrativeArea1' },
+    President: {role:'headOfState', level: 'country' },
+    }
   this.officialTitles = Object.keys(this.titles);
 }
+
+Agitator.prototype.getUserInfo = function(){
+  ask('What is your full name? ')
+    .then((answer)=>{
+      this.user.fullName = answer
+      stdout.write('Hello, ' + this.user.fullName + '! Now we need your address. ');
+      return ask('What is your street address? ');
+    })
+    .then((answer)=>{
+      this.user.street = answer;
+      return ask('Please enter apt #. If you don\'t have one, press enter ');
+    })
+    .then((answer) => {
+      this.user.apt = answer;
+      return ask('What city do you live in? ');
+    })
+    .then((answer) => {
+      this.user.city = answer;
+      return ask('What is the abbreviation of your state? ');
+    })
+    .then((answer)=>{
+      this.user.state = answer;
+      return ask('What is your zipcode? ')
+    })
+    .then((answer) =>{
+      this.user.zip = answer
+      this.user.address = this.user.street + ' ' + this.user.city + ' ' + this.user.state + ' ' + this.user.zip;
+      stdout.write(this.user.address)
+    })
+    .catch(err => console.log(err))
+}
+
+
