762811b0f1e587e78a584971765e2de0a23dda5c
colbisaurusrex <c.barthelmess1@gmail.com>
2017-04-22 13:10:13 -0400

    define Agitator constructor function

---
+++ withlibraries/app.js
@@ -0,0 +1,19 @@
+const prompt = require('readline-sync')
+const axios = require('axios')
+
+const Agitator = function(){
+  this.official = null;
+  this.userMessage = null;
+  this.fetchedOfficial = {};
+  this.fetchStatus = false;
+  this.user = {};
+  this.titles = {
+  'US House Representative': {role: 'legislatorLowerBody', level:'country'},
+  'US Congress Representative': {role: 'legislatorUpperBody', level: 'country'},
+  'State House Representative': {role: 'legislatorLowerBody', level:'administrativeArea1'},
+  'State Congress Representative': {role: 'legislatorUpperBody', level: 'administrativeArea1'},
+  Governor: {role: 'headOfGovernment', level: 'administrativeArea1' },
+  President: {role:'headOfState', level: 'country' },
+  }
+  this.officialTitles = Object.keys(this.titles);
+}
